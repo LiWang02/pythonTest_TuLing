713f7701ce8dcc74ad9829d9c81478274442b0e9
Liwang02 <wl1583542308@126.com>
2018-12-17 20:05:18 +0800

    add

--- OOP/ClassTest.py
+++ OOP/ClassTest.py
@@ -5,6 +5,7 @@ class PythonStudent():
     #必须赋值
     name=None
     age=None
+    __sex="girl"
     course="Python"
     #需要注意
     #1、def doHomeWork的缩进层级
@@ -13,6 +14,13 @@ class PythonStudent():
     def doHomeWork(self):
         print(self.name+"在做作业")
         return None
+    def say(self,name):
+        print("{0} is saying".format(name))
+        print("My name is{0}".format(self.name))
+        return None
+    def sayAgain():
+        print(__class__.name)
+        print("必须用类名进行调用")
 
 wangli=Student()
 #实例化
@@ -21,3 +29,6 @@ print(yueyue.name)
 print(yueyue.age)
 print(yueyue.course)
 print(PythonStudent.__dict__)
+yueyue.say(yueyue.name)
+PythonStudent.sayAgain()
+
